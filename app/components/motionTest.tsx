da7f398b870b9468d8fae459090501adc64fdeb9
Ed Orozco <eorozcoc@gmail.com>
2024-10-09 14:10:16 +0200

    got framer motion to work

--- app/components/motionTest.tsx
+++ app/components/motionTest.tsx
@@ -2,13 +2,27 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import { div } from "framer-motion/client";
 
 const MotionTest = () => {
     return (
-        <div>
-            <h1>Motion test</h1>
-            <motion.div className="w-10 h-10 bg-accent-primary rounded-full" />
-        </div>
+
+            <motion.div 
+            whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.1 },
+              }}
+              whileTap={{ 
+                scale: 0.95, 
+                transition: { duration: 0.1 },
+            }}
+
+                className="bg-stone-900 hover:bg-stone-800 shadow-lg shadow-stone-950/30 rounded-full px-8 py-3"
+            >  
+
+                <p className="text-stone-50 text-2xl">Press</p>
+            </motion.div>
+
     )
 }
 
