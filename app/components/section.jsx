8820fd23b856af9d3e498be91b5dc28280bffd97
Ed Orozco <eorozcoc@gmail.com>
2024-10-09 15:02:15 +0200

    got the full container fade in animation to work but not the staggerChildren

--- app/components/section.jsx
+++ app/components/section.jsx
@@ -1,9 +1,19 @@
+"use client"
+
 import React from "react";
+
+// Framer motion
+import { motion } from "framer-motion";
+import { div } from "framer-motion/client";
+import { useInView } from 'framer-motion';
+import { animate, stagger } from "framer-motion";
+
 
 const Section = ({title, description, children}) => {
     return (
         
-        <div className="flex flex-col gap-6">
+        <motion.div 
+        className="flex flex-col gap-6">
             <div className="flex flex-col gap-1 place-content-center">
                 <p className="label-primary">{title}</p>
                 <p className="label-secondary">{description}</p>
@@ -17,7 +27,7 @@ const Section = ({title, description, children}) => {
                 {children}
             </div>
 
-        </div>
+        </motion.div>
 
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+// Framer motion
 import { motion } from "framer-motion";
+import { div } from "framer-motion/client";
+import { useInView } from 'framer-motion';
+import { animate, stagger } from "framer-motion";
+
+
 import Section from "./components/section";
 import CalendarNotification from "./components/calendarNotification";
 import PageHeader from "./components/pageHeader";
 import MotionTest from "./components/motionTest";
 export default function Home() {
+
+
+
   return (
     <div className="flex flex-col justify-center min-h-screen bg-[background-color:var(--background)] font-[family-name:var(--font-geist-sans)] px-8">
           
@@ -11,15 +22,40 @@ export default function Home() {
 
       <PageHeader />
 
-      <div className="flex flex-col gap-16 py-8">
+{/* content container starts */}
+      <motion.div 
+      
 
-        <Section title="Calendar notification" description="Still figuring out the layout of this thing. Inspired by uilabs.dev."><CalendarNotification /></Section>
+        variants={{hidden: {opacity: 0, y: 20}, 
+        show: {
+          opacity: 1, y: 0, 
+          transition: {
+            staggerChildren: 0.1,
+          }, 
+        },
+      }}
+        initial="hidden"
+        animate="show"
+        className="flex flex-col gap-16 py-8"
+      >
 
-        <Section title="Framer motion button" description="Using whileHover and whileTap.">
-          <MotionTest />
-        </Section>
+        <motion.div
+        variants={{hidden: {opacity: 0}, show: {opacity: 1 } }}
+        >
+          <Section title="Calendar notification" description="Still figuring out the layout of this thing. Inspired by uilabs.dev."><CalendarNotification /></Section>
+        
+        </motion.div >
 
-      </div>
+        <motion.div
+        variants={{hidden: {opacity: 0}, show: {opacity: 1 } }}
+        >
+          <Section title="Framer motion button" description="Using whileHover and whileTap.">
+            <MotionTest />
+          </Section>
+          
+        </motion.div>
+
+      </motion.div>
      
       </main>
      
