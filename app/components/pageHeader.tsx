6586173359a731080c865b7b11fff20f68e369df
Ed <eorozcoc@gmail.com>
2024-10-11 16:40:38 +0200

    animated header

--- app/components/pageHeader.tsx
+++ app/components/pageHeader.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const entryVariants = {
+    hidden: {opacity: 0, y: 10},
+    show: {opacity: 1, y: 0}
+}
 
 const PageHeader = () => {
     return (
-        <div className="flex flex-col gap-1 py-16">
+        <motion.div
+        variants={entryVariants}
+        initial="hidden"
+        animate="show"
+        className="flex flex-col gap-1 py-16">
         <h1>Components.fun</h1>
         <h2>A space where I explore UI concepts</h2>
         <p className="label-secondary">Made by <a href="https://twitter.com/eddzio">@eddzio</a></p>
-      </div>   
+      </motion.div>   
     )
 }
 
