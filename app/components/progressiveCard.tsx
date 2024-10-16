f975b0418f4b514fe06006b63de180a53954d159
Ed Orozco <eorozcoc@gmail.com>
2024-10-16 16:01:58 +0200

    some animation but not quite the slide out I'm aiming for

--- app/components/progressiveCard.tsx
+++ app/components/progressiveCard.tsx
@@ -19,18 +19,43 @@ export const ProgressiveCard = () => {
         setShowFirstInput(true);
     };
 
+    const inputVariants = {
+        initial: { opacity: 0 },
+        animate: { opacity: 1, transition: { duration: 0.1 } }
+    }
+    
+
     return (
         <motion.div className="bg-card rounded-xl shadow-md border border-stone-200 dark:border-stone-800 w-[512px]">
             <div className="flex flex-col gap-2 items-left">
                 <div className="p-3 w-full border-b border-stone-200 dark:border-stone-700"><h1 className="text-base font-normal tracking-tight">Create a new channel</h1></div>
                 
                 <div className="p-3 w-full flex flex-col gap-4">    
-                    {/* Conditionally render the input fields */}
-                    {showFirstInput ? (
-                        <TextInput label="First" placeholder="Type something" />
-                    ) : (
-                        <TextInput label="Second" placeholder="Type something" />
-                    )}
+                    <AnimatePresence mode="wait">
+                        {showFirstInput ? (
+                            <motion.div
+                                key="first"
+                                variants={inputVariants}
+                                initial="initial"
+                                animate="animate"
+                                exit="exit"
+                                transition={{ duration: 0.3 }}
+                            >
+                                <TextInput label="First" placeholder="Type something" />
+                            </motion.div>
+                        ) : (
+                            <motion.div
+                                key="second"
+                                variants={inputVariants}
+                                initial="initial"
+                                animate="animate"
+                                exit="exit"
+                                transition={{ duration: 0.3 }}
+                            >
+                                <TextInput label="Second" placeholder="Type something" />
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
                 </div>
 
                 
