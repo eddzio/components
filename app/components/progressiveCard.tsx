ec3857aac7c73a4bfeae986b8bfbb882e853d770
Ed <eorozcoc@gmail.com>
2024-10-18 19:50:29 +0200

    removed the inputs and everything works but dont know how to customize the spring

--- app/components/progressiveCard.tsx
+++ app/components/progressiveCard.tsx
@@ -12,22 +12,16 @@ import { useSpring } from "framer-motion"
 export const ProgressiveCard = () => {
     const [showFirstInput, setShowFirstInput] = useState(true);
 
+
     const handleContinueClick = () => {
         setShowFirstInput(false);
-        handleCardSize(); // Add this line
 
     };
 
     const handleBackClick = () => {
         setShowFirstInput(true);
+
     };
-
-
-    const handleCardSize = () => {
-        setShowFirstInput(false);
-    }
-
-
 
     const cardSizeVariants = {
         one: { height: "185px" },
@@ -40,9 +34,10 @@ export const ProgressiveCard = () => {
 
     return (
         <motion.div 
+
         variants={cardSizeVariants}
         animate={showFirstInput ? "one" : "two"}
-        transition={{ spring: { stiffness: 100, damping: 10, mass: 1 }, duration: 0.1 }}
+        transition={{ duration: 0.15 }}
         className="bg-card rounded-xl
         border border-stone-200 dark:border-stone-700 w-full max-w-[400px]
         flex flex-col justify-between overflow-hidden
@@ -53,19 +48,20 @@ export const ProgressiveCard = () => {
                     <h1 className="text-base font-normal tracking-tight">Create a new channel</h1>
                     </div>
                 
-                <motion.div layout
-                className="p-3 w-full flex flex-col gap-2 ">    
+                <motion.div
+                className="p-3 w-full flex flex-col justify-between">    
                     <AnimatePresence mode="wait">
                         {showFirstInput ? (
                             <motion.div
-                            className="opacity-100"
+
                                 key="first"
                                 initial={{ opacity: 0, x: -100 }}
                                 animate={{ opacity: 1, x: 0 }}
                                 exit={{ opacity: 0, x: -100, transition: { duration: 0.1 } }}
-                                transition={{ duration: 0.1 }}
+                                transition={{ spring: { stiffness: 100, damping: 10, mass: 1 }, duration: 0.2 }}
                             >
-                                <TextInput label="First" placeholder="Type something" />
+                                <p className="text-base text-stone-900 dark:text-stone-50">First screen</p>
+                                <p className="text-base text-stone-500 dark:text-stone-400">It's simple and not very tall.</p>
                             </motion.div>
                         ) : (
                             <motion.div
@@ -73,22 +69,23 @@ export const ProgressiveCard = () => {
                                 initial={{ opacity: 0, x: 100 }}
                                 animate={{ opacity: 1, x: 0 }}
                                 exit={{ opacity: 0, x: 100, transition: { duration: 0.1 } }}
-                                transition={{ duration: 0.1 }}
+                                transition={{ spring: { stiffness: 100, damping: 10, mass: 1 }, duration: 0.2 }}
                             >
-                                <div className="flex flex-col gap-4">
-                                    <TextInput label="Second" placeholder="Type something" />
-                                    <TextInput label="Third" placeholder="Type something" />
-                                </div>
+
+                                <p className="text-base text-stone-900 dark:text-stone-50">Second screen</p>
+                                <p className="text-base text-stone-500 dark:text-stone-400">This one is similar to the first one, but it it's taller.</p>
+                                
+
+
                             </motion.div>
                         )}
                     </AnimatePresence>
                 </motion.div>
 
                 
-                <motion.div 
-                animate={showFirstInput ? "first" : "second"}
-                transition={{ duration: 0.6 }}
-                className="flex justify-start m-3 gap-3">
+                <motion.div layout
+                className="flex justify-start m-3 gap-3"
+                >
                     <ButtonSecondary 
                         label="Go back" 
                         onClick={handleBackClick}
