c6b22e052d5bf88e68846886ab9cf86848aea457
Ed <eorozcoc@gmail.com>
2024-11-18 05:59:21 +0100

    started iphone frame

--- app/components/dynamicBlob.tsx
+++ app/components/dynamicBlob.tsx
@@ -5,13 +5,13 @@ import { useState } from "react"
 
 
 
-const blobStyles = "bg-stone-800 hover:shadow-xl h-8 hover:cursor-pointer"
+const blobStyles = "bg-stone-800 hover:shadow-xl h-8 hover:cursor-pointer mx-auto"
 
 const blobVariants = {
     default: { width: 92, height: 32, borderRadius: "16px" },
     wide: { width: 156, height: 32, borderRadius: "16px" },
     large: { width: 256, height: 64, borderRadius: "64px" },
-    card: { width: 256, height: 312, borderRadius: "56  px" }
+    card: { width: 256, height: 312, borderRadius: "56px" }
 }
 
 export const DynamicBlob = () => {
@@ -25,7 +25,10 @@ export const DynamicBlob = () => {
     }
 
     return (
-        <div className="flex justify-center">
+        <div className="
+        grid grid-cols-1 content-start align-center bg-slate-200 h-full
+        rounded-t-[56px] pt-4 w-1/2 mt-8
+        ">
         <motion.div
             className={blobStyles}
             onClick={cycleVariant}
